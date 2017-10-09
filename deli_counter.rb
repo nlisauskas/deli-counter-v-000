d93202b48ed584568aeab486ba51166885fffdf5
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-09 15:42:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 def line(array)
   if array.length == 0 then puts "The line is currently empty."
   elsif array.length > 0 then
-    new_array = array.join("#{array.index + 1}. ")
+    new_array = array.join("#{array.index() + 1}. ")
     puts "The line is currently: #{new_array}"
   end
 end
