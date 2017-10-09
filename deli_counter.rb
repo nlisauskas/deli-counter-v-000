825352f608ba6d25b60db9ab7c5d4619c565f60e
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-09 15:37:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,9 @@ def line(array)
   if array.length == 0 then puts "The line is currently empty."
   elsif array.length > 0 then
     new_array = []
-    array.each do |x| 
+    array.each do |x|
       puts "#{x + 1}. #{array[x]}" << new_array
     end
   puts "The line is currently: #{new_array}"
   end
 end
-
-
