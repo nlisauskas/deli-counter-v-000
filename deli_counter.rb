7e10c3ace4bc43784d27959cd762930ed7e261a2
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-09 15:31:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0 then puts "The line is currently empty."
+  elsif array.length > 0 then 
+    new_array = []
+    x = 0
+    new_array = while x < array.length
+      puts "#{x + 1}. #{array[x]}"
+    x += 1
+  end
+  puts "The line is currently: #{new_array}"
+  
+  end
+end
