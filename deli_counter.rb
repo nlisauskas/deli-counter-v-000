d93e4c421c399a742a4a562718c2a66abdf9f9de
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-09 15:37:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,12 @@ katz_deli = []
 def line(array)
   if array.length == 0 then puts "The line is currently empty."
   elsif array.length > 0 then
-    x = 0
-    new_array = while x < array.length
-      "#{x + 1}. #{array[x]}"
-    x += 1
-  end
+    new_array = []
+    array.each do |x| 
+      puts "#{x + 1}. #{array[x]}" << new_array
+    end
   puts "The line is currently: #{new_array}"
   end
 end
+
+
