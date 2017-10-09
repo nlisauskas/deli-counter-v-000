9ef449b0a4c068ab0f0f34799a99399e7777971c
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-09 15:47:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     array.each.with_index(1) do |name, i|
-      current_line << "#{i}. #{name} "
+      current_line << " #{i}. #{name}"
     end
     puts current_line
   end
