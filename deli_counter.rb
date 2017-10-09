00601bb0e87dc03514f4e872c98c7919c98ad10f
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-09 15:55:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,5 @@ end
 
 def take_a_number(array, name)
   array << name
-  puts "Welcome, #{name}. You are number #{array.length} in line."
+  return "Welcome, #{name}. You are number #{array.length} in line."
 end
