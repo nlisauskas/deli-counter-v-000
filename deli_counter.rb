346a7d8578ad798171fce207776a9cf905c1915d
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-09 15:46:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    katz_deli.each.with_index(1) do |name, i|
+    array.each.with_index(1) do |name, i|
       current_line << "#{i}. #{name}"
     end
     puts current_line
